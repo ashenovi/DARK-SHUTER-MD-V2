cb0cb57fb7bc4da96eba945abf965e688bd07484
ashenovi <155998544+ashenovi@users.noreply.github.com>
2025-01-27 02:51:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: 'DARK-SHUTER-MD=sMQzSLCS#n8r_bbG704VJrjD_rK2ykJLru1LF28FQ9xCZkookMuc',
+SESSION_ID: 'DARK-SHUTER-MD=qWAzlJxY#zRx4TY2T0SAG7aDOk3DgJUbnERG8Spny0ZMI11eQr00',
 GITHUB_AUTH_TOKEN: 'YQhoA80xajpd7NdKCdAAOJ2ltY9dNx3TAGXD', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
 GITHUB_USER_NAME: 'ashenovi',
 
